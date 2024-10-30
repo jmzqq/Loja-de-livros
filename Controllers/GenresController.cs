7e0d47434c6a3b259485393360386d940aad37d3
jmzqq <joaomwguel@gmail.com>
2024-10-30 17:02:03 -0300

    feat: implement genres tab with MVC architecture and database connection

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,28 @@
+﻿using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class GenresController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Genre> genres = new List<Genre>
+            {
+                new Genre
+                { 
+                    Id = 1,
+                    Name = "Romance"
+                },
+
+                new Genre
+                {
+                    Id = 2,
+                    Name = "Ficção Científica"
+                }
+            };
+
+            return View(genres);
+        }
+    }
+}
